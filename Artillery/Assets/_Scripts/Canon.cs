341eb744b216119c6205dffcde1563d01aab9305
Jesusxd96 <33816389+Jesusxd96@users.noreply.github.com>
2024-12-15 20:39:40 -0600

    Implementacion New Input System
    - Se han configurado los inputs necesarios para el prototipo.
    - Se genero el script referente a estos inputs.
    - Se cambio el disparo y moviemiento de la torreta al nuevo input system.

--- Artillery/Assets/_Scripts/Canon.cs
+++ Artillery/Assets/_Scripts/Canon.cs
@@ -14,6 +14,27 @@ public class Canon : MonoBehaviour
     private GameObject puntaCanon;//Donde se instanciara la bala
     private float rotacion;
 
+    public CanonControls canonControls;
+    private InputAction apuntar;
+    private InputAction modificarFuerza;
+    private InputAction disparar;
+
+    private void Awake()
+    {
+        canonControls = new CanonControls();
+    }
+    private void OnEnable()
+    {   //Primero se le asignan las acciones del action map deseado a las variables InputAction
+        apuntar = canonControls.Canon.Apuntar;
+        modificarFuerza = canonControls.Canon.ModificarFuerza;
+        disparar = canonControls.Canon.Disparar;
+        //Luego estas se deben de habilitar
+        apuntar.Enable();
+        modificarFuerza.Enable();
+        disparar.Enable();
+        disparar.performed += Disparar;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,35 +46,41 @@ public class Canon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rotacion += Input.GetAxis("Horizontal") * GameManager.velocidadRotacion;
-        if(rotacion<=90 && rotacion >= 0)
+        rotacion += apuntar.ReadValue<float>() * GameManager.velocidadRotacion;
+        //Se le pasa el valor de Apuntar y se cambia la rotacion en base a eso
+        if (rotacion<=90 && rotacion >= 0)
         {
             transform.eulerAngles = new Vector3(rotacion, 90, 0.0f);//Se hace asi por como esta posicionado el canon
         }
         if (rotacion > 90) rotacion = 90;
         if (rotacion < 0) rotacion = 0;
-        if (GameManager.disparosRestantes != 0)//Si los disparos no son 0, puedes disparar
-        {//Se que no es la solucion mas optima, pero funciona de momento sin cambiar mucho el codigo
-            if (Input.GetKeyDown(KeyCode.Space)&&!bloqueado)
-            {
-                GameObject temp = Instantiate(balaPrefab, puntaCanon.transform.position, transform.rotation);
-                //GameObject Particula = Instantiate(ParticulasDisparo, puntaCanon.transform.position, transform.rotation);
-                Rigidbody tempRB = temp.GetComponent<Rigidbody>();
-                SeguirCamara.objetivo = temp;
-                Vector3 direccionDisparo = transform.rotation.eulerAngles;
-                direccionDisparo.y = 90 - direccionDisparo.x;//Nuevamente, se hace en Z por la rotacion del gameobject
-                Vector2 direccionParticulas = new Vector3(-90 + direccionDisparo.x, 90, 0);
-                GameObject Particula = Instantiate(ParticulasDisparo, puntaCanon.transform.position, Quaternion.Euler(direccionParticulas), transform);
-                tempRB.velocity = direccionDisparo.normalized * GameManager.velocidadBala;
-                //SourceDisparo.PlayOneShot(clipDisparo);
-                SourceDisparo.Play();
-                GameManager.disparosRestantes -= 1;
-                bloqueado = true;
-            }
-        }
+        //if (GameManager.disparosRestantes != 0)//Si los disparos no son 0, puedes disparar
+        //{//Se que no es la solucion mas optima, pero funciona de momento sin cambiar mucho el codigo
+        //        //Disparar();
+        //} //Ya se agrego y limita las balas, sin embargo el tiempo entre bala y bala no se aplica ahora
         else
         {
             Debug.Log("F, te quedaste sin disparos");
+        }
+    }
+
+    private void Disparar(InputAction.CallbackContext context)
+    {//Lo de CallbackContext sirve para mandar a llamar al metodo cuando el contexto de disparar se este realizando/llamado
+        if (GameManager.disparosRestantes != 0)
+        {
+            GameObject temp = Instantiate(balaPrefab, puntaCanon.transform.position, transform.rotation);
+            //GameObject Particula = Instantiate(ParticulasDisparo, puntaCanon.transform.position, transform.rotation);
+            Rigidbody tempRB = temp.GetComponent<Rigidbody>();
+            SeguirCamara.objetivo = temp;
+            Vector3 direccionDisparo = transform.rotation.eulerAngles;
+            direccionDisparo.y = 90 - direccionDisparo.x;//Nuevamente, se hace en Z por la rotacion del gameobject
+            Vector2 direccionParticulas = new Vector3(-90 + direccionDisparo.x, 90, 0);
+            GameObject Particula = Instantiate(ParticulasDisparo, puntaCanon.transform.position, Quaternion.Euler(direccionParticulas), transform);
+            tempRB.velocity = direccionDisparo.normalized * GameManager.velocidadBala;
+            //SourceDisparo.PlayOneShot(clipDisparo);
+            SourceDisparo.Play();
+            GameManager.disparosRestantes -= 1;
+            bloqueado = true;
         }
     }
 }
