c1701cc07581bb0b3f06b42083989d8d64c9d8de
Jesusxd96 <33816389+Jesusxd96@users.noreply.github.com>
2025-01-01 19:13:51 -0600

    Menu Inicio
    - Se modifico la Escena "Menu Inicio" para que esta sea un menu de inicio para un videojuego tal cual.
    - Se creo un script llamada "MainMenuUI" donde se detallan las funciones del menu de inicio.
    - Se comprobo el funcionamiento del mismo.
    - Al Script en cuestion se le agrego la opcion de abrir un canvas para configurar las opciones del juego. (Este canvas/menu aun no esta implementado).
    - Se limpio la Scene dedicada al Menu de Inicio dejando solo los objetos necesarios para su funcionamiento.

---
+++ Artillery/Assets/_Scripts/MainMenuUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    /*Canvas a utilizarse en el menu principal*/
+    public GameObject OptionsCanvas;
+    //GameObject ConfirmationCanvas; //Canvas para confirmar si ya gg el juego o nel pastel.
+
+    public void Start()
+    {
+        OptionsCanvas.SetActive(false);
+        //ConfirmationCanvas.SetActive(false);
+    }
+    public void StartGame()
+    {   //Temporal por si se implementa una seleccion de niveles a futuro.
+        var NextLevel = SceneManager.GetActiveScene().buildIndex + 1;
+        if (SceneManager.sceneCountInBuildSettings > NextLevel)
+            SceneManager.LoadScene(NextLevel);
+        else
+            SceneManager.LoadScene(0);//El Main menu siempre sera la Scene 0
+    }
+    public void OptionsMenu()
+    {
+        //Muestra el menu de opciones
+        OptionsCanvas.SetActive(true);
+    }
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
